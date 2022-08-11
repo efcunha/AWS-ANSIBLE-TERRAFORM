917884b0f7089bcf5b2a0656c390774ec49038a4
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2022-08-11 10:30:14 -0400

    update

---
+++ env/Prod/ansible.sh
@@ -0,0 +1,55 @@
+cd /home/ubuntu
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+sudo python3 -m pip install ansible
+tee -a playbook.yml > /dev/null <<EOT
+- hosts: localhost
+  tasks:
+
+  - name: Instalando Python3, virtualenv
+    apt:
+      pkg: 
+      - python3
+      - virtualenv
+      update_cache: yes
+    become: yes
+
+  - name: Git Clone
+    ansible.builtin.git:
+      repo: https://github.com/alura-cursos/clientes-leo-api.git
+      dest: /home/ubuntu/tcc
+      version: master
+      force: yes
+
+  - name: Instalando Dependencia com pip (Django e Django Rest)
+    pip:
+      virtualenv: /home/ubuntu/tcc/venv
+      requirements: /home/ubuntu/tcc/requirements.txt 
+
+  - name: Verificando se o projeto ja existe
+    stat: 
+      path: /home/ubuntu/tcc/setup/settings.py
+    register: projeto
+
+  - name: Iniciando o Projeto
+    shell: '. /home/ubuntu/tcc/venv/bin/activate; django-admin startproject setup /home/ubuntu/tcc'
+    when: not projeto.stat.exists
+
+  - name: Alterando o hosts do settings
+    lineinfile:
+      path: /home/ubuntu/tcc/setup/settings.py
+      regexp: 'ALLOWED_HOSTS'
+      line: 'ALLOWED_HOSTS = ["*"]'
+      backrefs: yes
+
+  - name: Configurando o Banco de Dados
+    shell:  '. /home/ubuntu/tcc/venv/bin/activate; python /home/ubuntu/tcc/manage.py migrate'
+
+  - name: Carregando os Dados Iniciais
+    shell:  '. /home/ubuntu/tcc/venv/bin/activate; python /home/ubuntu/tcc/manage.py loaddata clientes.json'
+
+  - name: Iniciando o Servido
+    shell:  '. /home/ubuntu/tcc/venv/bin/activate; nohup python /home/ubuntu/tcc/manage.py runserver 0.0.0.0:8000 &'
+EOT
+
+ansible-playbook playbook.yml 
--- infra/main.tf
+++ infra/main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
@@ -21,6 +20,7 @@ resource "aws_launch_template" "maquina" {
     Name = "AWS Terraform Ansible"
   }
   security_group_names = [ var.grupoDeSeguranca ]
+  user_data = filebase64("ansible.sh")
 }
 
 resource "aws_key_pair" "chaveSSH" {
@@ -29,7 +29,7 @@ resource "aws_key_pair" "chaveSSH" {
 } 
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = ["${var.regiao_aws}a"]
+  availability_zones = ["${var.regiao_aws}a", "${var.regiao_aws}b"]
   name= var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
@@ -37,5 +37,33 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
-  
+  target_group_arns = aws_lb_target_group.alvoLoadBalancer.arn
+}
+ resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.regiao_aws}a"
+}
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.regiao_aws}b"
+}
+
+resource "aws_lb" "loadBalance    type = "forward"r" {
+  internal = false
+  subnets = [aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id]
+}
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name = "maquinaAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+resource "aws_default_vpc" "default" {
+}
+resource "aws_lb_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.l    type = "forward"oadBalancer.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"    
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
+  }
 }
